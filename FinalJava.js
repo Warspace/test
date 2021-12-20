01274310cd84f94a17e36a802fcc986c347d2cfc
Warspace <90347313+Warspace@users.noreply.github.com>
2021-12-19 17:24:29 -0700

    Add files via upload

---
+++ FinalJava.js
@@ -0,0 +1,15 @@
+//These are the functions for page transfers
+function goSuggestions(){
+    console.log("goSuggestions() started");
+    location.replace("Suggestions.html");
+}
+
+function goAnnouncments(){
+    console.log("goAnnouncments() started");
+    location.replace("FinalProjectHTML.html");
+}
+
+function goCommunity(){
+    console.log("goCommunity() started");
+    location.replace("Community.html");
+}
